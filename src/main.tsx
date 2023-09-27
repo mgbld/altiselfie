7cfda37ec278ae86616df7766cbe6d0e5d3ece13
Miguel Breceda de Lara <mbreceda@altimetrik.com>
2023-09-27 13:36:43 -0600

    Add and implement initial react-router-dom

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { RouterProvider, createBrowserRouter, createRoutesFromElements, Route } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
-import App from './App.tsx'
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route
+    path='/'
+    element={<div>Hello World</div>}
+    >
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')! as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-    <App />
+      <RouterProvider router={router} />
     </ChakraProvider>
   </React.StrictMode>,
 )
